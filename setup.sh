3ffaee2f359878260c8b47e677e5cbafbb29f8cb
DevMig <nerdxontheline@outlook.com>
2020-08-06 19:41:08 +0100

    Update to Rust Setup Script!

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ sudo apt install build-essential
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 rustc --version
+rustup update
+rustup component add rls rust-analysis rust-src
